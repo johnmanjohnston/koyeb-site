f7f8d278a348a46da373d02a2975dbeb96cc54ab
johnmanjohnston <c.ethanjohn@gmail.com>
2024-10-01 21:02:51 +0400

    counter

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const fs = require("fs");
+var path = require("path");
 
 app.get('/', (req, res) => {
+  var c = null;
+  
+  c = Number(fs.readFileSync(path.join(__dirname, "count.txt"), {encoding: "utf8", flag: "r"}));
+
+  fs.writeFileSync(path.join(__dirname, "count.txt"), String(c + 1));
+
   res.json({
-    message: 'Hello, world!',
+    message: `count.txt: ${c}`,
   })
 })
 
